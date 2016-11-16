40f46cf3f4f9c95402b8915dd3a7f5b5792d9ffc
john-lay <john_lay@hotmail.co.uk>
2016-11-16 20:56:29 +0000

    target framework changed to 4.5 and basic map drawn to DOM

---
+++ QuestIsometricDemo/Scripts/Logic/Main.ts
@@ -0,0 +1,104 @@
+﻿var map = [
+    // row 1
+    [
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+    ],
+    // row 2
+    [
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+    ],
+    // row 3
+    [
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+    ],
+    // row 4
+    [
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+    ],
+    // row 5
+    [
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+    ],
+    // row 6
+    [
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+    ],
+    // row 7
+    [
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+    ],
+    // row 8
+    [
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+        { height: 1, blocked: false, canMove: false },
+    ],
+];
+
+// write to DOM
+var table = '<table>';
+
+for (var i = 0; i < map.length; i++) {
+    table += '<tr>';
+    for (var j = 0; j < map[i].length; j++) {
+        table += '<td>' + i + ', ' + j + '</td>';
+    }
+    table += '</tr>';
+}
+
+table += '</table>';
+document.write(table);
