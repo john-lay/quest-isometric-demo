8b4c821c133b2e387714b498c229c3486485df9b
john-lay <john_lay@hotmail.co.uk>
2017-03-20 10:22:44 +0000

    added missing status ctrl and job enum

---
+++ QuestIsometricDemo/Scripts/Game/Controllers/StatusController.ts
@@ -0,0 +1,41 @@
+﻿/// <reference path="../../typings/angularjs/angular.d.ts" />
+module StatusModule {
+    'use strict';
+
+    export interface IStatusScope extends ng.IScope {
+        // PROPERTIES
+        swords: Weapons.IWeapon[];
+        jobs: IEnumViewModel[];
+
+        vm: {
+            job: Job;
+            weapon: Weapons.IWeapon;
+        }
+
+        // PUBLIC METHODS
+
+    }
+
+    // Debug only
+    export var statusScope;
+
+    export class StatusController {
+
+        static $inject = ["$scope", "HelperService"];
+
+        constructor($scope: IStatusScope, helperService: HelperModule.IHelperService) {
+            
+            // Debug only
+            statusScope = $scope;
+
+            $scope.swords = Weapons.swords;
+            $scope.jobs = helperService.enumToViewModel(Job);
+            $scope.vm = {
+                job: null,
+                weapon: null
+            }
+        }
+    }
+}
+
+app.controller("StatusController", StatusModule.StatusController);
---
+++ QuestIsometricDemo/Scripts/Game/Enums/Job.ts
@@ -0,0 +1,54 @@
+﻿enum Job {
+    // HUME
+    Soldier,
+    WhiteMage,
+    BlackMage,
+    Thief,
+    Archer,
+    Fighter,
+    Paladin,
+    Ninja,
+    Hunter,
+    BlueMage,
+    Illusionist,
+
+    // BANGAA
+    Warrior,
+    WhiteMonk,
+    Gladiator,
+    Defender,
+    Dragoon,
+    Bishop,
+    Templar,
+
+    // NU MOU
+    Beastmaster,
+    //WhiteMage,
+    //BlackMage,
+    Alchemist,
+    TimeMage,
+    //Illusionist,
+    Sage,
+    Morpher,
+
+    // VIERA
+    Fencer,
+    //WhiteMage,
+    //Archer,
+    RedMage,
+    Elementalist,
+    Sniper,
+    Assassin,
+    Summoner,
+
+    // MOOGLE
+    Animist,
+    //BlackMage,
+    //Thief,
+    Gunner,
+    MogKnight,
+    Juggler,
+    //TimeMage,
+    Gadgeteer
+
+}
